b5aa1bf5570a64b419a45c0c1db862dc84ace88d
Vadim Dvorovenko <Vadimon@mail.ru>
2020-07-09 03:42:14 +0700

    version and requirements

---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Book from znanium.com module
+ *
+ * @package mod_znaniumcombook
+ * @copyright COPYRIGHT
+ * @license LICENSE
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2020070801; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2016120500; // Requires Moodle 3.2 for AMD Modal
+$plugin->component = 'mod_znaniumcombook';  // Full name of the plugin (used for diagnostics)
+$plugin->dependencies = array(
+    'block_znanium_com' => 2020060601,
+);
